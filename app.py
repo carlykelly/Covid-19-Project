5214cfe10ebe0c62f1e08954dd20f304fbf1641a
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-07-30 04:56:39 -0400

    Census data and mongo stuff

--- app.py
+++ app.py
@@ -13,79 +13,102 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 
-# Replace with our Mondgo
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-# mongo = PyMongo(app)
+from pymongo import MongoClient
+import json
+client = MongoClient('localhost', 27017)
 
-# createing route that renders index.html template
-@app.route("/")
-def home():    
-    return render_template("index.html")
+# Inserting state data
+db = client['population_db']
+collection_state = db['state_population']
 
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    conn = engine.connect()
+with open('state_pop.json') as f:
+    file_data = json.load(f)
+collection_state.insert_many(file_data)
 
-    if request.method == "POST":
-        name = request.form["petName"]
-        pet_type = request.form["petType"]
-        age = request.form["petAge"]
+# Inserting county data
+collection_county = db['county_population']
+with open('state_county_pop.json') as f:
+    file_data1 = json.load(f)
+collection_county.insert_many(file_data1)
 
-        pets_df = pd.DataFrame({
-            'name': [name],
-            'type': [pet_type],
-            'age': [age]
-        })
+client.close()
 
-        pets_df.to_sql('pets', con=conn, if_exists='append', index=False)
 
-        return redirect("/", code=302)
 
-    conn.close()
 
-    return render_template("form.html")
+# # Replace with our Mondgo
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/population_db"
+# # mongo = PyMongo(app)
 
-@app.route("/api/pals-summary")
-def pals_summary():
-    conn = engine.connect()
+# # createing route that renders index.html template
+# @app.route("/")
+# def home():    
+#     return render_template("index.html")
+
+# # Query the database and send the jsonified results
+# @app.route("/send", methods=["GET", "POST"])
+# def send():
+#     conn = engine.connect()
+
+#     if request.method == "POST":
+#         name = request.form["petName"]
+#         pet_type = request.form["petType"]
+#         age = request.form["petAge"]
+
+#         pets_df = pd.DataFrame({
+#             'name': [name],
+#             'type': [pet_type],
+#             'age': [age]
+#         })
+
+#         pets_df.to_sql('pets', con=conn, if_exists='append', index=False)
+
+#         return redirect("/", code=302)
+
+#     conn.close()
+
+#     return render_template("form.html")
+
+# @app.route("/api/pals-summary")
+# def pals_summary():
+#     conn = engine.connect()
     
-    query = '''
-        SELECT 
-            type,
-            COUNT(type) as count
-        FROM
-            pets
-        GROUP BY
-            type
-    ''' 
+#     query = '''
+#         SELECT 
+#             type,
+#             COUNT(type) as count
+#         FROM
+#             pets
+#         GROUP BY
+#             type
+#     ''' 
 
-    pets_df = pd.read_sql(query, con=conn)
+#     pets_df = pd.read_sql(query, con=conn)
 
-    pets_json = pets_df.to_json(orient='records')
+#     pets_json = pets_df.to_json(orient='records')
 
-    conn.close()
+#     conn.close()
 
-    return pets_json
+#     return pets_json
 
-@app.route("/api/pals")
-def pals():
-    conn = engine.connect()
+# @app.route("/api/pals")
+# def pals():
+#     conn = engine.connect()
     
-    query = '''
-        SELECT 
-            *
-        FROM
-            pets
-    ''' 
+#     query = '''
+#         SELECT 
+#             *
+#         FROM
+#             pets
+#     ''' 
 
-    pets_df = pd.read_sql(query, con=conn)
+#     pets_df = pd.read_sql(query, con=conn)
 
-    pets_json = pets_df.to_json(orient='records')
+#     pets_json = pets_df.to_json(orient='records')
 
-    conn.close()
+#     conn.close()
 
-    return pets_json
+#     return pets_json
 
 if __name__ == "__main__":
     app.run(debug=True)
