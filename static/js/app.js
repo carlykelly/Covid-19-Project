7f9a08a5f2bb67ffe9b7a967edc0a45007aac4e2
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-07-31 16:27:41 -0400

    mapbox GL in HTML

--- static/js/app.js
+++ static/js/app.js
@@ -9,40 +9,63 @@ function updateDash(){
         var State_Population = data
         console.log(State_Population)
 
-        
-        
 
         d3.select('form').on('change',function(d){
             var userSelection=d3.select("#state-selector").node().value;
             console.log(userSelection)
-        });
-      
-        //d3.select("select").on("change",function(d){
-        //userSelection.on("change",function(){
-        
-            //var newSelection=d3.event.target.value;
-            //console.log(newSelection)
+            statePop=State_Population.filter(s=>s.State==userSelection)
+            console.log(statePop)
 
             //saving keys to variables
-            var StateName = State_Population[0].State
+            var StateName = statePop[0].State
             //console.log(StateName)
-            var StatePopulation = State_Population[0].Population
-            var StateLatitude = State_Population[0].Latitude
-            var StateLongitude = State_Population[0].Longitude
+            var StatePopulation = statePop[0].Population
+            var StateLatitude = statePop[0].Latitude
+            var StateLongitude = statePop[0].Longitude
+
+           
 
             //State Bio Div
             d3.select("#state-name").text(`State: ${StateName}`).style("color","blue")
             d3.select("#population").text(`Population: ${StatePopulation}`).style("color","blue")
 
 
-            // Gauge
-            //var rate_of_spread=NumTests/NumCases
-        //})
+            
+        })
 
 
     })
 
     d3.json(county_url).then((data)=>{
+        console.log(data)
+
+    })
+
+
+    d3.json(county_url).then((data)=>{
+        console.log(data)
+
+        mapboxgl.accessToken = API_KEY;
+
+        var map = new mapboxgl.Map({
+        container:'state-timeseries', // container element id
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-74.0059, 40.7128], // initial map center in [lon, lat]
+        zoom: 12
+        });
+
+
+
+
+
+
+
+
+        // Gauge
+            //var rate_of_spread=NumTests/NumCases
+        //State Bio Div
+        //d3.select("#state-name").text(`State: ${StateName}`).style("color","blue")
+        //d3.select("#population").text(`Population: ${StatePopulation}`).style("color","blue")
 
     })
 }
---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+
+// API key
+const API_KEY = "sk.eyJ1IjoiY2xlbWlibGFjIiwiYSI6ImNrY3pkZWJidzBpcTAycXJ5N3hjZzNsaDMifQ.zDv-qU_BgSuqGTnZjWNk9Q";
