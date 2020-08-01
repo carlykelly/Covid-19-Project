22097fa684ac49bc719bf49709d594cf62cd9f26
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-08-01 19:52:35 -0400

    still working on honeycomb

--- static/js/app.js
+++ static/js/app.js
@@ -8,7 +8,7 @@ function updateDash(){
 
     
     //Honey Comb
-    Highcharts.chart('country-timeseries', {
+    Highcharts.chart('country-honeycomb', {
         chart: {
             type: 'tilemap',
             inverted: true,
@@ -16,7 +16,12 @@ function updateDash(){
         },
     
         accessibility: {
-            description: 'A tile map represents the states of the USA by population in 2016. The hexagonal tiles are positioned to geographically echo the map of the USA. A color-coded legend states the population levels as below 1 million (beige), 1 to 5 million (orange), 5 to 20 million (pink) and above 20 million (hot pink). The chart is interactive, and the individual state data points are displayed upon hovering. Three states have a population of above 20 million: California (39.3 million), Texas (27.9 million) and Florida (20.6 million). The northern US region from Massachusetts in the Northwest to Illinois in the Midwest contains the highest concentration of states with a population of 5 to 20 million people. The southern US region from South Carolina in the Southeast to New Mexico in the Southwest contains the highest concentration of states with a population of 1 to 5 million people. 6 states have a population of less than 1 million people; these include Alaska, Delaware, Wyoming, North Dakota, South Dakota and Vermont. The state with the lowest population is Wyoming in the Northwest with 584,153 people.',
+            description: 'The tile map represents the states of the USA and their total number\
+             of positive Corona virus cases.The hexagonal tiles are positioned to geographically\
+             echo the map of the USA. A color-coded legend states the positive case levels\
+             as below 1 million (beige), 1 to 5 million (orange),5 to 20 million (pink)\
+             and above 20 million (hot pink). The chart is interactive, and the individual\
+             state data points are displayed upon hovering.',
             screenReaderSection: {
                 beforeChartFormat:
                     '<h5>{chartTitle}</h5>' +
@@ -30,7 +35,7 @@ function updateDash(){
         },
     
         title: {
-            text: 'U.S. states by population in 2016'
+            text: 'U.S. states by total number of positve Corona virus cases.'
         },
 
     
@@ -91,357 +96,357 @@ function updateDash(){
                 region: 'South',
                 x: 6,
                 y: 7,
-                value: 4849377
+                value: 3134228
             }, {
                 'hc-a2': 'AK',
                 name: 'Alaska',
                 region: 'West',
                 x: 0,
                 y: 0,
-                value: 737732
+                value: 108299
             }, {
                 'hc-a2': 'AZ',
                 name: 'Arizona',
                 region: 'West',
                 x: 5,
                 y: 3,
-                value: 6745408
+                value: 5780908
             }, {
                 'hc-a2': 'AR',
                 name: 'Arkansas',
                 region: 'South',
                 x: 5,
                 y: 6,
-                value: 2994079
+                value: 1539705
             }, {
                 'hc-a2': 'CA',
                 name: 'California',
                 region: 'West',
                 x: 5,
                 y: 2,
-                value: 39250017
+                value: 19207190
             }, {
                 'hc-a2': 'CO',
                 name: 'Colorado',
                 region: 'West',
                 x: 4,
                 y: 3,
-                value: 5540545
+                value: 2998384
             }, {
                 'hc-a2': 'CT',
                 name: 'Connecticut',
                 region: 'Northeast',
                 x: 3,
                 y: 11,
-                value: 3596677
+                value: 4388525
             }, {
                 'hc-a2': 'DE',
                 name: 'Delaware',
                 region: 'South',
                 x: 4,
                 y: 9,
-                value: 935614
+                value: 1006764
             }, {
                 'hc-a2': 'DC',
                 name: 'District of Columbia',
                 region: 'South',
                 x: 4,
                 y: 10,
-                value: 7288000
+                value: 914117
             }, {
                 'hc-a2': 'FL',
                 name: 'Florida',
                 region: 'South',
                 x: 8,
                 y: 8,
-                value: 20612439
+                value: 14016659
             }, {
                 'hc-a2': 'GA',
                 name: 'Georgia',
                 region: 'South',
                 x: 7,
                 y: 8,
-                value: 10310371
+                value: 6942391
             }, {
                 'hc-a2': 'HI',
                 name: 'Hawaii',
                 region: 'West',
                 x: 8,
                 y: 0,
-                value: 1419561
+                value: 97440
             }, {
                 'hc-a2': 'ID',
                 name: 'Idaho',
                 region: 'West',
                 x: 3,
                 y: 2,
-                value: 1634464
+                value: 631656
             }, {
                 'hc-a2': 'IL',
                 name: 'Illinois',
                 region: 'Midwest',
                 x: 3,
                 y: 6,
-                value: 12801539
+                value: 12490874
             }, {
                 'hc-a2': 'IN',
                 name: 'Indiana',
                 region: 'Midwest',
                 x: 3,
                 y: 7,
-                value: 6596855
+                value: 4078381
             }, {
                 'hc-a2': 'IA',
                 name: 'Iowa',
                 region: 'Midwest',
                 x: 3,
                 y: 5,
-                value: 3107126
+                value: 2366798
             }, {
                 'hc-a2': 'KS',
                 name: 'Kansas',
                 region: 'Midwest',
                 x: 5,
                 y: 5,
-                value: 2904021
+                value: 1290020
             }, {
                 'hc-a2': 'KY',
                 name: 'Kentucky',
                 region: 'South',
                 x: 4,
                 y: 6,
-                value: 4413457
+                value: 1365731
             }, {
                 'hc-a2': 'LA',
                 name: 'Louisiana',
                 region: 'South',
                 x: 6,
                 y: 5,
-                value: 4649676
+                value: 57322696
             }, {
                 'hc-a2': 'ME',
                 name: 'Maine',
                 region: 'Northeast',
                 x: 0,
                 y: 11,
-                value: 1330089
+                value: 268805
             }, {
                 'hc-a2': 'MD',
                 name: 'Maryland',
                 region: 'South',
                 x: 4,
                 y: 8,
-                value: 6016447
+                value: 5696893
             }, {
                 'hc-a2': 'MA',
                 name: 'Massachusetts',
                 region: 'Northeast',
                 x: 2,
                 y: 10,
-                value: 6811779
+                value: 10140701
             }, {
                 'hc-a2': 'MI',
                 name: 'Michigan',
                 region: 'Midwest',
                 x: 2,
                 y: 7,
-                value: 9928301
+                value: 6790230
             }, {
                 'hc-a2': 'MN',
                 name: 'Minnesota',
                 region: 'Midwest',
                 x: 2,
                 y: 4,
-                value: 5519952
+                value: 2798608
             }, {
                 'hc-a2': 'MS',
                 name: 'Mississippi',
                 region: 'South',
                 x: 6,
                 y: 6,
-                value: 2984926
+                value: 2294571
             }, {
                 'hc-a2': 'MO',
                 name: 'Missouri',
                 region: 'Midwest',
                 x: 4,
                 y: 5,
-                value: 6093000
+                value: 2005918
             }, {
                 'hc-a2': 'MT',
                 name: 'Montana',
                 region: 'West',
                 x: 2,
                 y: 2,
-                value: 1023579
+                value: 114170
             }, {
                 'hc-a2': 'NE',
                 name: 'Nebraska',
                 region: 'Midwest',
                 x: 4,
                 y: 4,
-                value: 1881503
+                value: 1525892
             }, {
                 'hc-a2': 'NV',
                 name: 'Nevada',
                 region: 'West',
                 x: 4,
                 y: 2,
-                value: 2839099
+                value: 1650121
             }, {
                 'hc-a2': 'NH',
                 name: 'New Hampshire',
                 region: 'Northeast',
                 x: 1,
                 y: 11,
-                value: 1326813
+                value: 492211
             }, {
                 'hc-a2': 'NJ',
                 name: 'New Jersey',
                 region: 'Northeast',
                 x: 3,
                 y: 10,
-                value: 8944469
+                value: 17115258
             }, {
                 'hc-a2': 'NM',
                 name: 'New Mexico',
                 region: 'West',
                 x: 6,
                 y: 3,
-                value: 2085572
+                value: 1011270
             }, {
                 'hc-a2': 'NY',
                 name: 'New York',
                 region: 'Northeast',
                 x: 2,
                 y: 9,
-                value: 19745289
+                value: 41571810
             }, {
                 'hc-a2': 'NC',
                 name: 'North Carolina',
                 region: 'South',
                 x: 5,
                 y: 9,
-                value: 10146788
+                value: 4920440
             }, {
                 'hc-a2': 'ND',
                 name: 'North Dakota',
                 region: 'Midwest',
                 x: 2,
                 y: 3,
-                value: 739482
+                value: 309928
             }, {
                 'hc-a2': 'OH',
                 name: 'Ohio',
                 region: 'Midwest',
                 x: 3,
                 y: 8,
-                value: 11614373
+                value: 4539737
             }, {
                 'hc-a2': 'OK',
                 name: 'Oklahoma',
                 region: 'South',
                 x: 6,
                 y: 4,
-                value: 3878051
+                value: 1210800
             }, {
                 'hc-a2': 'OR',
                 name: 'Oregon',
                 region: 'West',
                 x: 4,
                 y: 1,
-                value: 3970239
+                value: 750875
             }, {
                 'hc-a2': 'PA',
                 name: 'Pennsylvania',
                 region: 'Northeast',
                 x: 3,
                 y: 9,
-                value: 12784227
+                value: 8450949
             }, {
                 'hc-a2': 'RI',
                 name: 'Rhode Island',
                 region: 'Northeast',
                 x: 2,
                 y: 11,
-                value: 1055173
+                value: 1515607
             }, {
                 'hc-a2': 'SC',
                 name: 'South Carolina',
                 region: 'South',
                 x: 6,
                 y: 8,
-                value: 4832482
+                value: 2915267
             }, {
                 'hc-a2': 'SD',
                 name: 'South Dakota',
                 region: 'Midwest',
                 x: 3,
                 y: 4,
-                value: 853175
+                value: 565146
             }, {
                 'hc-a2': 'TN',
                 name: 'Tennessee',
                 region: 'South',
                 x: 5,
                 y: 7,
-                value: 6651194
+                value: 3794491
             }, {
                 'hc-a2': 'TX',
                 name: 'Texas',
                 region: 'South',
                 x: 7,
                 y: 4,
-                value: 27862596
+                value: 14122466
             }, {
                 'hc-a2': 'UT',
                 name: 'Utah',
                 region: 'West',
                 x: 5,
                 y: 4,
-                value: 2942902
+                value: 1722012
             }, {
                 'hc-a2': 'VT',
                 name: 'Vermont',
                 region: 'Northeast',
                 x: 1,
                 y: 10,
-                value: 626011
+                value: 125178
             }, {
                 'hc-a2': 'VA',
                 name: 'Virginia',
                 region: 'South',
                 x: 5,
                 y: 8,
-                value: 8411808
+                value: 5041543
             }, {
                 'hc-a2': 'WA',
                 name: 'Washington',
                 region: 'West',
                 x: 2,
                 y: 1,
-                value: 7288000
+                value: 3194672
             }, {
                 'hc-a2': 'WV',
                 name: 'West Virginia',
                 region: 'South',
                 x: 4,
                 y: 7,
-                value: 1850326
+                value: 280451
             }, {
                 'hc-a2': 'WI',
                 name: 'Wisconsin',
                 region: 'Midwest',
                 x: 2,
                 y: 5,
-                value: 5778708
+                value: 2573030
             }, {
                 'hc-a2': 'WY',
                 name: 'Wyoming',
                 region: 'West',
                 x: 3,
                 y: 3,
-                value: 584153
+                value: 129511
             }]
         }]
     });
@@ -651,8 +656,8 @@ function updateDash(){
 
             // Sorting dictionary
             function compare(a, b) {
-                const stateCasesA = a.cases;
-                const stateCasesB = b.cases;
+                const stateCasesA = a.state;
+                const stateCasesB = b.state;
               
                 let comparison = 0;
                 if (stateCasesA > stateCasesB) {
@@ -663,12 +668,10 @@ function updateDash(){
                 return comparison;
             }
             //Using this for honey comb
-            var sort_cases_asc=casesStateTotals.sort(compare)
+            var sort_state_asc=casesStateTotals.sort(compare)
             console.log("Total cases by State");
-            console.log(sort_cases_asc);
-
-
-              
+            console.log(sort_state_asc);
+     
 
         })
 
