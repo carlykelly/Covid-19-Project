86668fefe6c9b3a819699aab7deba0bd7906285e
Carly Kelly <carlyfkelly@gmail.com>
2020-08-05 00:00:41 -0400

    changing API Key

--- static/js/app_CK.js
+++ static/js/app_CK.js
@@ -11,7 +11,7 @@ var myMap = L.map("total-county-cloropleth", {
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/light-v9",
-    accessToken: 'sk.eyJ1IjoiY2FybHlma2VsbHkiLCJhIjoiY2tjeHZ2cHNkMDQwZjJxcGtybXI0dmNtNiJ9.klVCz6NGP6-H6CYGBpFyfA'
+    accessToken: API_Key
   }).addTo(myMap);
   
   // Load in geojson data
@@ -51,55 +51,33 @@ var myMap = L.map("total-county-cloropleth", {
   
     }).addTo(myMap);
   
-    // // Set up the legend
-    // var legend = L.control({ position: "bottomright" });
-    // legend.onAdd = function() {
-    //   var div = L.DomUtil.create("div", "info legend");
-    //   var limits = geojson.options.limits;
-    //   var colors = geojson.options.colors;
-    //   var labels = [];
-  
-    //   // Add min & max
-    //   var legendInfo = "<h3>Percentage of Residents Dying of Covid</h3>" +
-    //     "<div class=\"labels\">" +
-    //       "<div class=\"min\">" + limits[0] + "</div>" +
-    //       "<div class=\"max\">" + (limits[limits.length - 1]).toFixed(3) + "</div>" +
-    //     "</div>";
-  
-    //   div.innerHTML = legendInfo;
-  
-    //   limits.forEach(function(limit, index) {
-    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    //   });
-  
-    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    //   return div;
-    
-  
-    // }
-    // legend.addTo(myMap)
-
+    // Set up the legend
     var legend = L.control({ position: "bottomright" });
     legend.onAdd = function() {
       var div = L.DomUtil.create("div", "info legend");
       var limits = geojson.options.limits;
       var colors = geojson.options.colors;
       var labels = [];
+  
       // Add min & max
-      var legendInfo = "<h1>Median Income</h1>" +
+      var legendInfo = "<h3>Percentage of Residents Dying of Covid</h3>" +
         "<div class=\"labels\">" +
           "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+          "<div class=\"max\">" + (limits[limits.length - 1]).toFixed(3) + "</div>" +
         "</div>";
+  
       div.innerHTML = legendInfo;
+  
       limits.forEach(function(limit, index) {
         labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
       });
+  
       div.innerHTML += "<ul>" + labels.join("") + "</ul>";
       return div;
-    };
-    // Adding legend to the map
-    legend.addTo(myMap);
+    
+  
+    }
+    legend.addTo(myMap)
   
   }).catch(function(err) {
       console.warn(err);
