236f752bd0be21ed70708a6dddc4a110097d93ca
Carly Kelly <carlyfkelly@gmail.com>
2020-08-05 16:45:07 -0400

    zooming in as state selected works

--- static/js/app_CK.js
+++ static/js/app_CK.js
@@ -176,7 +176,7 @@ var baseMaps
           accessToken: API_Key
         }).addTo(myMap);
       
-        L.control.layers(baseMaps).addTo(myMap);
+        L.control.layers(baseMaps, null, {collapsed:false}).addTo(myMap);
 
         myMap.on('baselayerchange',function(eventlayer){
           this.removeControl(legend)
@@ -193,63 +193,102 @@ var baseMaps
           }
         })
 
-        
+        d3.select('form').on('change', function(d){
+          d3.json(stateData).then(function(data2){
+            //console.log(data2.features[0].properties)
+            
 
-        // if(myMap.layers == cases){
-        //   var legend = L.control({ position: "bottomright" });
-        //   legend.onAdd = function() {
-        //     var div = L.DomUtil.create("div", "info legend");
-        //     var limits = cases.options.limits;
-        //     var colors = cases.options.colors;
-        //     var labels = [];
-        //     // Add min & max
-        //     var legendInfo = "<h6>Percentage of Residents Contracting Covid</h6>" +
-        //       "<div class=\"labels\">" +
-        //         "<div class=\"min\">" + limits[0] + "</div>" +
-        //         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        //       "</div>";
-        //     div.innerHTML = legendInfo;
-        //     limits.forEach(function(limit, index) {
-        //       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-        //     });
-        //     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        //     return div;
-        //   };
-        //   // Adding legend to the map
-        //   legend.addTo(myMap);
-        // }
-        
-        // if(myMap.layers == patient_mortality){
-        //   var legend = L.control({ position: "bottomright" });
-        //   legend.onAdd = function() {
-        //     var div = L.DomUtil.create("div", "info legend");
-        //     var limits = patient_mortality.options.limits;
-        //     var colors = patient_mortality.options.colors;
-        //     var labels = [];
-        //     // Add min & max
-        //     var legendInfo = "<h6>Percentage of Coivd Patients who have Died</h6>" +
-        //       "<div class=\"labels\">" +
-        //         "<div class=\"min\">" + limits[0] + "</div>" +
-        //         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        //       "</div>";
-        //     div.innerHTML = legendInfo;
-        //     limits.forEach(function(limit, index) {
-        //       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-        //     });
-        //     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        //     return div;
-        //   };
-        //   // Adding legend to the map
-        //   legend.addTo(myMap);
-        // }
+          state = d3.select("#state-selector").node().value;
+          //console.log(state)
+          var poly = data2.features.filter(a => a.properties.name == state)
+          //console.log(poly)
+          var coordinates = stateCoordDict[state]
+          //console.log(coordinates)
+          var zoom = myMap.options.zoom
+          var graphCoords = myMap.options.center
+          //console.log(graphCoords)
+          //console.log(zoom)
+          graphCoords = coordinates
+          //myMap.panTo(new L.LatLng(coordinates[0],coordinates[1]))
+          if(state != 'us' && state != 'Alaska'){
+            //myMap.setZoom(7)
+            myMap.setView(coordinates, 7)
+            var stateCoords = []
+            var lotsOfCords = poly[0].geometry.coordinates
+            for(i=0; i<lotsOfCords.length; i++){
+              stateCoords.push(lotsOfCords[i])
+            }
+            console.log(stateCoords[0])
+            L.polygon(stateCoords[0],
+              {
+                color: "blue",
+                fillColor: "blue",
+                fillOpacity: 100
+              }).addTo(myMap)
+          }
+          else{
+            //myMap.setZoom(4)
+            myMap.setView(coordinates, 4)
+          }
+
+        })
+      })
+    })
   
-    })//.addTo(myMap);
-  
+stateCoordDict = {
+  "Alabama": [32.3182, -86.9023 ],
+  "Alaska": [64.2008, -149.4937],
+  "Arizona": [34.0489, -111.0937],
+  "Arkansas": [35.2010, -91.8318],
+  "California": [36.7783, -119.4179],
+  "Colorado": [39.5501, -105.7821],
+  "Connecticut": [41.6032, -73.0877],
+  "Delaware": [38.9108, -75.5277],
+  "Florida": [27.6648, -81.5158],
+  "Georgia": [32.1656, -82.9001],
+  "Hawaii": [19.8968, -155.5828],
+  "Idaho": [44.0682, -114.7420],
+  "Illinois": [40.6331, -89.3985],
+  "Indiana": [40.2672, -40.2672],
+  "Iowa": [41.8780, -93.0977],
+  "Kansas": [39.0119, -98.4842],
+  "Kentucky": [37.8393, -84.2700],
+  "Louisiana": [30.9843, -91.9623],
+  "Maine": [45.2538, -69.4455],
+  "Maryland": [39.0458, -76.6413],
+  "Massachusetts": [42.407211, -71.382439],
+  "Michigan": [44.3148, -85.6024],
+  "Minnesota": [46.7296, -94.6859],
+  "Mississippi": [32.3547, -89.3985],
+  "Missouri": [37.9643, -91.8318],
+  "Montana": [46.8797, -110.3626],
+  "Nebraska": [41.4925, -99.9018],
+  "Nevada": [38.8026, -116.4194],
+  "New Hampshire": [43.1939, -71.5724] ,
+  "New Jersey": [40.0583, -74.4057],
+  "New Mexico": [34.5199, -105.8701],
+  "New York": [40.7128, -74.0060],
+  "North Carolina": [35.7596, -79.0193],
+  "North Dakota": [47.5515, -101.0020],
+  "Ohio": [40.4173, -82.9071],
+  "Oklahoma": [35.0078, 97.0929],
+  "Oregon": [43.8041, -120.5542],
+  "Pennsylvania": [41.2033, -77.1945],
+  "Rhode Island": [41.5801, -71.4774],
+  "South Carolina": [33.8361, -81.1637],
+  "South Dakota": [43.9695, -99.9018],
+  "Tennessee": [35.5175, -86.5804],
+  "Texas": [31.9686, -99.9018],
+  "Utah": [39.3210, -111.0937],
+  "Vermont": [44.5588, -72.5778],
+  "Virginia": [37.4316, -78.6569],
+  "Washington": [47.7511, -120.7401],
+  "West Virginia": [38.5976, -80.4549],
+  "Wisconsin": [43.7844, -88.7879],
+  "Wyoming": [43.0760, -107.2903],
+  "us": [38.7003, -93.7095]
+}
 
+var stateData = "static/js/state_geojson.json"
 
-
-  
-  //})//.catch(function(err) {
-      //console.warn(err);
-  //});
 
