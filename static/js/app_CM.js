a46beb0e7959959b87d52d866b2870777d270e74
unknown <cmikus6@gmail.com>
2020-08-08 03:25:31 -0400

    dynamic honey_index

--- static/js/app_CM.js
+++ static/js/app_CM.js
@@ -3,6 +3,10 @@ d3.select('form').on('change', function(d){
     if(userSelection != 'us'){
     var stateNameCases = d3.select("#state-name-cases").text(`Counties of ${userSelection} with Highest Death Rate by Reported Cases`);
     var stateNameDeaths = d3.select("#state-name-deaths").text(`Counties of ${userSelection} with Highest Death Rate by Total Population`);
+    var comparison = d3.select("#data-comp-state").text(`Comparison of ${userSelection} Data`);
+    var reliability = d3.select("#reliability-state").text(`Reliability Scores for ${userSelection}`);
+    var seriesNameCases = d3.select("#cases-series-state").text(`Timeseries of Cases Reported by NYT and Atlantic for ${userSelection}`);
+    var seriesNameDeaths = d3.select("#death-series-state").text(`Timeseries of Deaths Reported by NYT and Atlantic for ${userSelection}`);
     }
 })
 
